3c555b5e0c237548e78671a56380de4420f22237
Remco Stoeten <remcostoeten@hotmail.com>
2025-11-29 19:43:42 +0100

    feat: implement open collection and global search functionality
    - Add seed import dialog to Index page for opening collections
    - Create GlobalSearchDialog component for searching all notes
    - Fix syntax error in initialize-defaults.ts (stray localUsageNoteSeed)
    - Fix folder creation logic to use folder.parentFolderName correctly
    - Fix Tailwind CSS warning in editor-wrapper.tsx
    - Rename RawMDXEditor.tsx to raw-mdx-editor.tsx (kebab-case)
    - Add search feature integration to app layout container

--- src/features/notes/seeds/defaults/index.ts
+++ src/features/notes/seeds/defaults/index.ts
@@ -8,6 +8,11 @@ export { releasesFolderSeed } from './releases-folder'
 export { installNoteSeed } from './todo/install-note'
 export { localUsageNoteSeed } from './todo/local-usage-note'
 export { taskInEditorNoteSeed } from './todo/task-in-editor'
+export { vimKeybindingsNoteSeed } from './todo/vim-keybindings'
+export { fontSettingsNoteSeed } from './todo/font-settings'
+export { pinAndFavoriteItemsNoteSeed } from './todo/pin-and-favorite-items'
+export { multiSelectBulkOperationsNoteSeed } from './todo/multi-select-bulk-operations'
+export { shiftClickRangeSelectionNoteSeed } from './todo/shift-click-range-selection'
 
 // Release notes
 export { releaseNote20251125Seed } from './releases/2025-11-25-linked-mentions-tabs'
