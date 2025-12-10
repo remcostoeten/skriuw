718d926904196946c4ab5a0b3df57ba6f1646bb1
Remco Stoeten <remcostoeten@hotmail.com>
2025-12-10 03:29:13 +0100

    chore: Remove obsolete seed files and documentation

--- apps/web/features/notes/split-view/constants.ts
+++
@@ -1,6 +0,0 @@
-export const NOTE_TAB_DRAG_TYPE = 'application/x-skriuw-note-tab'
-
-export type NoteTabDragPayload = {
-	noteId: string
-	sourcePaneId: string | null
-}
--- packages/ui/use-toast.ts
+++
@@ -1,1 +0,0 @@
-export { useToast, toast } from '@skriuw/core-logic'
