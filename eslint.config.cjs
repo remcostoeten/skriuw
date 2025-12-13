37fb27d3409d80582a3f8e3aa2543fe278a719c5
Remco Stoeten <remcostoeten@hotmail.com>
2025-12-13 11:51:18 +0100

    Configure FlatCompat with recommended presets

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -1,7 +1,12 @@
 const { FlatCompat } = require('@eslint/eslintrc')
+const js = require('@eslint/js')
 const base = require('@skriuw/config/eslint/base')
 
-const compat = new FlatCompat({ baseDirectory: __dirname })
+const compat = new FlatCompat({
+	baseDirectory: __dirname,
+	recommendedConfig: js.configs.recommended,
+	allConfig: js.configs.all
+})
 
 module.exports = [
 	{
